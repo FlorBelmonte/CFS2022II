9c05bf2b430925a3f50cd062360943c78f37275d
florbelmonte <mariaflorenciabelmonterojas@gmail.com>
2022-11-10 12:43:01 -0300

    Entrega de ejercicios de Interfaces y abstractas

---
+++ Entregable Interfaces/ejercicio1.js
@@ -0,0 +1,11 @@
+// interface Auto {
+//     arrancar(): void;
+//     acelerar(): void;
+// }
+var relojSumergible = /** @class */ (function () {
+    function relojSumergible() {
+    }
+    return relojSumergible;
+}());
+var relojSwach = new relojSumergible(30, 3);
+console.log(relojSwach);
