86018e725925cc5cf1db7b526052ef654245585e
florbelmonte <mariaflorenciabelmonterojas@gmail.com>
2022-11-21 21:34:34 -0300

    Entrega de ejercicios manejo de Errores

---
+++ Entregable Manejo de Errores/archivo.js
@@ -0,0 +1,63 @@
+// class Alumno {
+//     private nombre: string;
+//     private nota: number;
+//     private DNI: number;
+//     public constructor(nombre: string, nota: number, DNI: number) {
+//         this.nombre = nombre;
+//         this.nota = nota;
+//         this.DNI = DNI;
+//     }
+//     public getNota() {
+//         return this.nota;
+//     }
+//     public estaAprobado() {
+//         if(this.nota <0){
+//             throw new Error("Los numeros negativos no son validos como nota")
+//         } else if (this.nota>0 && this.nota<7){
+//             console.log("El alumno esta desaprobado")
+//         }else if(this.nota>=7 && this.nota <=10) {
+//             console.log("el alumno esta aprobado")
+//         } else if (this.nota > 10){
+//             throw new Error ("Las notas mayores a 10 no son validas")
+//         }
+//         }
+//     }
+//     let alumno1 : Alumno = new Alumno('Gloria Dominguez', -8, 252546346);
+//     let alumno2 : Alumno = new Alumno('Juan Dominguez', 5, 25254255);
+//     let alumno3 : Alumno = new Alumno('Gloria Dominguez', 8, 252546346);
+//     let alumno4 : Alumno = new Alumno('Juan Dominguez', 15, 25254255);
+//     try {
+//         alumno1.estaAprobado();
+//     }catch(error){
+//         console.log("dato invalido:" + error.message)
+//     }
+// ----------------------------------------------------------------------------------------------
+var Operario = /** @class */ (function () {
+    function Operario(nombre, sueldo) {
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+    }
+    Operario.prototype.getSueldo = function () {
+        return this.sueldo;
+    };
+    Operario.prototype.AumentoSueldo = function () {
+        var nuevoSueldo = 0;
+        if (this.sueldo < 80000) {
+            throw new Error("El sueldo no puede ser inferior a ese monto");
+        }
+        else if (this.sueldo > 80.000) {
+            nuevoSueldo = this.sueldo * 1.05;
+        }
+        return nuevoSueldo;
+    };
+    return Operario;
+}());
+var trabajador1 = new Operario("Marcos", 84000);
+var trabajador2 = new Operario("Juan", 76000);
+try {
+    trabajador2.AumentoSueldo();
+}
+catch (error) {
+    console.log("ocurrio un error esperado..");
+}
+console.log(trabajador2.AumentoSueldo());
